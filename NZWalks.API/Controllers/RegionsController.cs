eeeea7202a18627c716934e065bf9afd299864d4
FERNANDO K.B.R.N <kbrnfernando96@outlook.com>
2023-12-01 21:55:17 +0530

    Added DTOs

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RegionsController : ControllerBase
+{
+    private readonly NZWalksDbContext _dbContext;
+
+    public RegionsController(NZWalksDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var regionsDomain = _dbContext.Regions.ToList();
+
+        var regionsDto = new List<RegionDto>();
+        foreach (var regionDomain in regionsDomain)
+        {
+            regionsDto.Add(new RegionDto()
+            {
+                Id = regionDomain.Id,
+                Name = regionDomain.Name,
+                Code = regionDomain.Code,
+                RegionImageUrl = regionDomain.RegionImageUrl,
+            });
+        }
+
+        return Ok(regionsDto);
+    }
+
+
+    [HttpGet]
+    [Route("{id:Guid}")]
+    public IActionResult GetById([FromRoute] Guid id)
+    {
+        var regionDomain = _dbContext.Regions.FirstOrDefault(r => r.Id == id);
+
+        if (regionDomain == null)
+        {
+            return NotFound();
+        }
+
+        var regionDto = new RegionDto()
+        {
+            Id = regionDomain.Id,
+            Name = regionDomain.Name,
+            Code = regionDomain.Code,
+            RegionImageUrl = regionDomain.RegionImageUrl,
+        };
+        return Ok(regionDto);
+    }
+}
